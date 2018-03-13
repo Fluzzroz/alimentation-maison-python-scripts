05837c2595e8f1b989540da89e96806064319f50
Benoît Boucher <boucher.benoit@gmail.com>
2018-03-13 15:48:28 -0400

    Now the distance between Door ID 0 and any location is set to 0, which will be useful if we need to cancel meetings. Added an argument parser. Added a lot more printed info

--- tsp.py
+++ tsp.py
@@ -28,19 +28,23 @@ from ortools.constraint_solver import routing_enums_pb2
 import pandas as pd
 import numpy as np
 import math
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--meet_length', default = 60, type = int, help='Length of time between two consecutive meetings in minutes.')
+parser.add_argument('--weight_strength', default = 2, type = int, help='How much should Skills & Interests affect the routes. Put 0 for no effect, in which case raw distance will be calculated.')
 
 
 class CreateDistanceCallback(object):
     """Create callback to calculate distances between points, weighted or not."""
 
-    def __init__(self, locations, interests, skills, weight_strength=0):
+    def __init__(self, locations, interests, skills, weight_strength):
         """Initialize distance array.
         If you want the weight, you need to supply all 3 parameters
         interest, skill, and weight_strength. For quickly switching between
         raw (unweighted) and weighted distance, just set weight_strength to 0."""
         size = len(locations)
         num_vehicle = len(skills)
-        depot = 0
         self.matrix = {}
         for vehicle in range(num_vehicle):
             self.matrix[vehicle] = {}
@@ -49,16 +53,17 @@ class CreateDistanceCallback(object):
                 for to_node in range(size):
                     self.matrix[vehicle][from_node][to_node] = {}
                     weight = 1 + weight_strength * abs(interests[to_node] - skills[vehicle])
+                    x1 = locations[from_node][0]
+                    y1 = locations[from_node][1]
+                    x2 = locations[to_node][0]
+                    y2 = locations[to_node][1]
+
                     # Define the distance from the depot to any node to be 0.
-                    if from_node == depot or to_node == depot:
+                    # The depot location is defined by Door ID 0, location = [0,0]
+                    if x1 == 0 or x2 == 0:
                         self.matrix[vehicle][from_node][to_node] = 0
                     else:
-                        x1 = locations[from_node][0]
-                        y1 = locations[from_node][1]
-                        x2 = locations[to_node][0]
-                        y2 = locations[to_node][1]
                         self.matrix[vehicle][from_node][to_node] = int(self.earth_distance(x1, y1, x2, y2) * weight)
-
 
     def distance(self, from_node, to_node):
         return self.matrix[0][from_node][to_node]
@@ -111,9 +116,55 @@ class CreateTimeCallback(object):
         return self.time_matrix
 
 
-def main():
+def import_data(meet_length):
+    """imports data; meet_length is the time between each meeting in minutes"""
+    df_salesmen = pd.read_csv("tsp-python-import - salesmen.csv")
+    df_locations = pd.read_csv("tsp-python-import - locations.csv")
+    df_meetings = pd.read_csv("tsp-python-import - meetings.csv",
+                              parse_dates=[1], infer_datetime_format=True)
+
+    # add the coordinate info to meetings
+    df_meetings = pd.merge(df_meetings, df_locations, how="left",
+                           left_on="meet_location", right_on="door_id",
+                           sort=False, validate="m:1")
+
+    # convert the schedule from datetime value to a chronometered value in
+    # minutes starting from the depot at 0, thus first meeting starts at 60
+    # ASSUMPTION: ALL MEETINGS LAST THE SAME
+    node_to_time = df_meetings["meet_time"].values
+    # add Home before any meeting
+    home_time = np.min(node_to_time) - np.timedelta64(meet_length, "[m]")
+    node_to_time = np.concatenate(([home_time], node_to_time))
+    chronometer = (node_to_time - home_time).astype("timedelta64[m]")
+    chronometer = chronometer.astype("int64")
+
+    # matrix, each row is a pair of coordinates
+    location = df_meetings[["door_latitude", "door_longitude"]].values
+    # add Home coordinates
+    location = np.concatenate(([[0, 0]], location), axis=0)
+
+    # maps a node to a door_id
+    node_to_door = df_meetings["meet_location"].values
+    # add the Home node
+    node_to_door = np.concatenate(([0], node_to_door))
+
+    # map routes to salesman
+    route_to_salesman = df_salesmen["salesman_name"].values
+
+    # skill is the quality of the salesman, interest is the quality of the meeting
+    # better salesman gets better meeting
+    skill = df_salesmen["salesman_skill"].values
+    interest = df_meetings["meet_interest"].values
+    # add Home interest
+    interest = np.concatenate(([0], interest))
+
+    return [location, chronometer, skill, interest, node_to_door,
+            route_to_salesman, node_to_time]
+
+
+def main(args):
     # import data
-    data = import_data(meet_length=60)
+    data = import_data(args.meet_length)
     locations = data[0]
     chronometer = data[1]
     skills = data[2]
@@ -137,16 +188,17 @@ def main():
         search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
 
         # Callback to the distance function.
-        dist_between_locations = CreateDistanceCallback(locations, interests, skills, weight_strength=2)
+        dist_between_locations = CreateDistanceCallback(locations, interests, skills, args.weight_strength)
 
-        #adding the cost function for each vehicle seperately since each
-        #salesman has a different skill that we will optimize for
+        # adding the cost function for each vehicle seperately since each
+        # salesman has a different skill that we will optimize for
         dist_callbacks = []
         for vehicle in range(num_vehicles):
             dist_callbacks.append(dist_between_locations.distance_v(vehicle))
 
         for vehicle in range(num_vehicles):
             routing.SetArcCostEvaluatorOfVehicle(dist_callbacks[vehicle], vehicle)
+
 
         # Put a callback to the time.
         times_at_locations = CreateTimeCallback(meet_length)
@@ -158,7 +210,7 @@ def main():
         total_work_time = int(max(chronometer) + meet_length)  #enough to return Home
         routing.AddDimension(times_callback, time_slack, total_work_time, True, time_d_name)
 
-        #Add the time windows constraint, which is the meeting schedule
+        # Add the time windows constraint, which is the meeting schedule
         time_dimension = routing.GetDimensionOrDie(time_d_name)
         for location in range(1, num_locations):
             start = int(chronometer[location] - time_slack)
@@ -181,6 +233,7 @@ def main():
                 route_door = "Door ID: "
                 route_interest = "Meet Quality:"
                 total_interest = 0
+                route_arc = "Arc cost: "
 
                 while not routing.IsEnd(index_next):
                     node_index = routing.IndexToNode(index)
@@ -196,6 +249,7 @@ def main():
                     index = index_next
                     index_next = assignment.Value(routing.NextVar(index))
                     total_interest += interests[node_index]
+                    route_arc += str(dist_callbacks[vehicle_nbr](node_index, node_index_next)) + " -> "
 
                 node_index = routing.IndexToNode(index)
                 node_index_next = routing.IndexToNode(index_next)
@@ -205,11 +259,13 @@ def main():
                 route_schedule += str(node_to_time[node_index])[11:-13] + " -> " + str(node_to_time[node_index_next])[11:-13]
                 route_interest += str(interests[node_index]) + " -> " + str(interests[node_index_next])
                 total_interest += interests[node_index]
+                route_arc += str(dist_callbacks[vehicle_nbr](node_index, node_index_next))
 
                 print("\nRoute for Salesman: " + str(route_to_salesman[vehicle_nbr]) + "(skill=" + str(skills[vehicle_nbr]) + ")")
                 print(route)
                 print(route_interest)
                 print(route_door)
+                print(route_arc)
                 print(route_schedule)
                 print("Weighted Distance of Route " + str(vehicle_nbr) + ": " + str(route_dist))
                 print("Total Quality serviced: " + str(total_interest))
@@ -220,51 +276,8 @@ def main():
         print('Specify an instance greater than 0.')
 
 
-def import_data(meet_length):
-    """imports data; meet_length is the time between each meeting in minutes"""
-    df_salesmen = pd.read_csv("tsp-python-import - salesmen.csv")
-    df_locations = pd.read_csv("tsp-python-import - locations.csv")
-    df_meetings = pd.read_csv("tsp-python-import - meetings.csv",
-                              parse_dates=[1], infer_datetime_format=True)
-
-    # add the coordinate info to meetings
-    df_meetings = pd.merge(df_meetings, df_locations, how="left",
-                           left_on="meet_location", right_on="door_id",
-                           sort=False, validate="1:1")
-
-    # convert the schedule from datetime value to a chronometered value in
-    # minutes starting from the depot at 0, thus first meeting starts at 60
-    # ASSUMPTION: ALL MEETINGS LAST THE SAME
-    node_to_time = df_meetings["meet_time"].values
-    #add Home before any meeting
-    home_time = np.min(node_to_time) - np.timedelta64(meet_length, "[m]")
-    node_to_time = np.concatenate(([home_time], node_to_time))
-    chronometer = (node_to_time - home_time).astype("timedelta64[m]")
-    chronometer = chronometer.astype("int64")
-
-    # matrix, each row is a pair of coordinates
-    location = df_meetings[["door_latitude", "door_longitude"]].values
-    # add Home coordinates
-    location = np.concatenate(([[0, 0]], location), axis=0)
-
-    # maps a node to a door_id
-    node_to_door = df_meetings["meet_location"].values
-    # add the Home node
-    node_to_door = np.concatenate(([0], node_to_door))
-
-    # map routes to salesman
-    route_to_salesman = df_salesmen["salesman_name"].values
-
-    # skill is the quality of the salesman, interest is the quality of the meeting
-    # better salesman gets better meeting
-    skill = df_salesmen["salesman_skill"].values
-    interest = df_meetings["meet_interest"].values
-    #add Home interest
-    interest = np.concatenate(([0], interest))
-
-    return [location, chronometer, skill, interest, node_to_door,
-            route_to_salesman, node_to_time]
-
-
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    args.weight_strength = 0
+    args.meet_length = 60
+    main(args)
